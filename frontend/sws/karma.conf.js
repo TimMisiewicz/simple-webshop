84a9e7034c9fbceb8845baa65a21fdd1188edce6
Tim Misiewicz <70705167+TimMisiewicz@users.noreply.github.com>
2022-01-08 15:14:53 +0100

    chore: fixed wrong install

--- frontend/sws/karma.conf.js
+++ frontend/sws/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/swsfrontend'),
+      dir: require('path').join(__dirname, './coverage/sws'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- frontend/sws/src/app/app.component.spec.ts
+++ frontend/sws/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'swsfrontend'`, () => {
+  it(`should have as title 'sws'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('swsfrontend');
+    expect(app.title).toEqual('sws');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('swsfrontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('sws app is running!');
   });
 });
